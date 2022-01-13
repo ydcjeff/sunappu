20396ebfffbdf980b172b2b446887e0db4ce2f3b
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-01-13 23:54:53 +0630

    ci: add --allow-read

--- mod.ts
+++ mod.ts
@@ -2,6 +2,29 @@ import { assertStrictEquals } from 'https://deno.land/std@0.121.0/testing/assert
 import * as path from 'https://deno.land/std@0.121.0/path/mod.ts';
 import * as fs from 'https://deno.land/std@0.121.0/fs/mod.ts';
 
+/**
+ * Setup snapshot directory and file and dynamically import as ES Module.
+ *
+ * The imported module can have a number of named exports to compare.
+ * If not, it will write the string version of `actual` to it.
+ *
+ * _**PLEASE REVIEW TO THE CHANGES CAREFULLY.**_
+ *
+ * @param file The current test file, it should always be `import.meta.url`.
+ * @returns (actual, name) function
+ * - `actual` should be the actual object to stringify.
+ * - `name` to use as the named export in the snapshot module.
+ *
+ * ```ts
+ * import { setupSnapshot } from 'https://deno.land/x/sunappu/mod.ts';
+ *
+ * const assertSnapshot = await setupSnapshot(import.meta.url);
+ *
+ * test('...', () => {
+ *    assertSnapshot(actual, 'named export')
+ *  })
+ * ```
+ */
 export async function setupSnapshot(file: string) {
   file = path.fromFileUrl(path.normalize(file));
   const fname = path.basename(file);
